5c6552c0b7ba184420c37550d1e125608c143588
chattie <christuaik@gmail.com>
2023-02-24 20:10:10 -0800

    content added

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-/*
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-*/
+const bar = document.getElementsByClassName("resumeNav")[0];
+console.log(bar);
+let i = 0;
+
+const colors = ["#FFA3FD", "#E384FF", "#865DFF", "#191825", null]
+
+function rotateColors() {
+    bar.style.backgroundColor = colors[i];
+    if (i < colors.length) {
+        i += 1;}
+    else {i = 0;}
+}
+
+document.addEventListener("keydown", function(event){
+        rotateColors();
+    })
